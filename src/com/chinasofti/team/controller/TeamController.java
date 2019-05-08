a691746f2428e0e99f476aa376cfd3e98e5dba03
安逸 <1508708139@qq.com>
2019-05-08 10:21:34 +0800

    修改组团实体

---
+++ src/com/chinasofti/team/controller/TeamController.java
@@ -0,0 +1,14 @@
+package com.chinasofti.team.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @Description TeamController
+ * @Author WYR
+ * @CreateTime 2019-05-08 10:20
+ */
+@Controller
+@RequestMapping(name = "team")
+public class TeamController {
+}
